2d39c039fb84c75132a33ac826f8f6eda28b9212
Reider14 <haider14guilherme@gmail.com>
2024-12-08 18:59:22 -0100

    Up the file.tsx for create components of CSS

---
+++ AppStyled.tsx
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const Espaco = styled.div`
+background-color: red;
+width: 150px;
+height: 150px;
+display: flex;
+flex-direction: row;
+align-items: center;
+justify-content: center;
+`;
+
+export const Texto = styled.h1`
+color: black;
+font-size: 30px;
+`;
+
+export const Quadrado = styled.div`
+    width: 200px;
+    background: black;
+    hight: 200px;
+`;
